8defe45341797dc5ea896f43bcd92d2d5365bd57
immxalan <aedocoding@gmail.com>
2020-11-09 22:34:55 -0800

    Added endpoints for adding pins, updating pins, deleting pins. Todo: getting a users pins

--- api/server.js
+++ api/server.js
@@ -2,12 +2,14 @@ const express = require("express");
 const helmet = require("helmet");
 const CORS = require("cors");
 const authRouter = require("../auth/auth-router");
+const pinRouter = require("../pins/pin-router");
 
 const server = express();
 server.use(helmet());
 server.use(CORS());
 server.use(express.json());
 server.use("/auth", authRouter);
+server.use("/pins", pinRouter)
 
 server.get("/", (req, res) => {
   res.status(200).json({ message: "Server is running" });
---
+++ pins/pin-router.js
@@ -0,0 +1,53 @@
+const router = require("express").Router();
+const pins = require("./pin-model");
+const authenticate = require("../auth/authenticate-middleware");
+
+router.post("/add", authenticate, (req, res) => {
+  const newPin = req.body;
+  pins
+    .add(newPin)
+    .then((pin) => {
+      res.status(201).json([{ message: "Pin added:" }, pin]);
+    })
+    .catch((err) => {
+      res.status(500).json({ message: `Error adding pin: ${err}` });
+    });
+});
+router.put("/edit/:id", authenticate, (req, res) => {
+  const pinChanges = req.body;
+  const { id } = req.params;
+  pins
+    .update(id, pinChanges)
+    .then((e) => {
+      if (e) {
+        res
+          .status(200)
+          .json([{ message: "Pin updated with the following changes:" }, e]);
+      } else {
+        res.status(404).json({ message: "The pin could not be found" });
+      }
+    })
+    .catch((err) => {
+      res
+        .status(500)
+        .json({ message: `There was an error updating the pin: ${err}` });
+    });
+});
+router.delete("/delete/:id", authenticate, (req, res) => {
+  const { id } = req.params;
+  pins
+    .remove(id)
+    .then((pin) => {
+      if (pin > 0) {
+        res
+          .status(200)
+          .json({ message: `Pin with id: ${id} successfully deleted` });
+      } else {
+        res.status(404).json({ message: "The pin could not be found" });
+      }
+    })
+    .catch((err) => {
+      res.status(500).json({ message: ` Error removing the pin: ${err}` });
+    });
+});
+module.exports = router;
